08d1f782481ec77bc729781ddb001affb43ff50c
Zoha Nafees <zohanafees09@gmail.com>
2024-06-01 23:22:23 +0500

    First Push

---
+++ main.ts
@@ -0,0 +1,9 @@
+//Name Cases: Store a person’s name in a variable, and then print that person’s name in lowercase, uppercase, and titlecase.
+
+let personName ="Zoha Nafees"
+//upper case
+console.log("uppercase:", personName.toUpperCase() )
+//lower case
+console.log("lowercase:", personName.toLowerCase())
+//title case
+console.log("title case:", personName.replace(/\b\w/g,c=> c.toUpperCase()))
